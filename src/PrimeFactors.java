48223f8b4b2606e91ba67c3b68f3146aa3c2a520
jamiefang17 <jamiefang17@gmail.com>
2023-11-07 15:46:41 -0800

    testOne complete

---
+++ src/PrimeFactors.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeFactors {
+    
+    public List<Integer> resolve(int value) {
+        return new ArrayList<Integer>();
+    }
+}
--- src/PrimeFactorsTest.java
+++ src/PrimeFactorsTest.java
@@ -1,7 +1,20 @@
-/**
- * PrimeFactorsTest
- */
-public class PrimeFactorsTest {
+//tests
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class PrimeFactorsTest {
+    @Test
+    public void testOne(){
+        PrimeFactors primeFactors =new PrimeFactors();
+        List<Integer> expected = new ArrayList<Integer>();
+        List<Integer> actual = primeFactors.resolve(1);
+        assertEquals(expected, actual);
+
+    }
     
 }
