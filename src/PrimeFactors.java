61b476de1d1d8395c2f6fd9dcddae888f8f7b2a4
jamiefang17 <jamiefang17@gmail.com>
2023-11-15 11:32:32 -0800

    initial commit

--- src/PrimeFactors.java
+++ src/PrimeFactors.java
@@ -5,8 +5,17 @@ public class PrimeFactors {
     
     public List<Integer> resolve(int value) {
         List<Integer> result = new ArrayList<Integer>();
+        int candidate = 2;
+        while (value > 1) {
+            while (value % candidate == 0) {
+                result.add(candidate);
+                value /= candidate;
+                }
+                candidate++;
+}
+                
         if (value > 1){
-            result.add(2);
+            result.add(value);
         }
         return result;
     }
--- src/PrimeFactorsTest.java
+++ src/PrimeFactorsTest.java
@@ -7,21 +7,69 @@ import java.util.List;
 
 import org.junit.Test;
 
-public class PrimeFactorsTest {
-    @Test
-    public void testOne(){
-        PrimeFactors primeFactors =new PrimeFactors();
-        List<Integer> expected = new ArrayList<Integer>();
+public class PrimeFactorsTest 
+{
+/**
+* Generates a list of values from the provided sequence
+* @param values sequence of values to create a list of
+* @return list of values
+*/
+
+private static List<Integer> factorsOf(int value){
+    PrimeFactors primeFactors = new PrimeFactors();
+    return primeFactors.resolve(value);
+  }
+
+private static List<Integer> listOf(int... values){
+    List<Integer> result = new ArrayList<Integer>();
+    for (int value : values) {
+        result.add(value);
+    }
+    return result;
+    }
+
+
+
+ @Test
+    public void testOne()
+    {
+        PrimeFactors primeFactors = new PrimeFactors();
+        List<Integer> expected =new ArrayList<Integer>();
         List<Integer> actual = primeFactors.resolve(1);
         assertEquals(expected, actual);
-
     }
-    @Test
-    public void testTwo(){
-        PrimeFactors primeFactors =new PrimeFactors();
-        List<Integer> expected = new ArrayList<Integer>();
-        expected.add(2);
-        List<Integer> actual = primeFactors.resolve(2);
-        assertEquals(expected, actual);
-        }
+
+    
+   @Test
+   public void testTwo(){
+    List<Integer> expected = listOf(2);
+    List<Integer> actual = factorsOf(2);
+    assertEquals(expected, actual);
 }
+
+
+    @Test
+    public void testThree(){
+        assertEquals(listOf(3), factorsOf(3));
+    }
+
+    @Test
+    public void testFour() {
+        assertEquals(listOf(2, 2), factorsOf(4));
+        }
+    
+     @Test
+    public void testFive() {
+        assertEquals(listOf(2, 3), factorsOf(6));
+        }
+
+    @Test
+    public void testSix() {
+        assertEquals(listOf(2, 2, 2), factorsOf(8));
+        }
+
+    @Test
+    public void testSeven() {
+        assertEquals(listOf(3, 3), factorsOf(9));
+        }
+    }
